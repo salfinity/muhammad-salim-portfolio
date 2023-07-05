f9966ff1511600cd7865d18f67b845e5a44aa703
mohsalim <moharts2000@gmail.com>
2023-07-06 00:53:07 +0300

    save project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
         <h2 className='py-4 text-center'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
-            title='Property Finder'
+            title='Portfolio Website'
             backgroundImg={propertyImg}
             projectUrl='/property'
             tech='React JS'
